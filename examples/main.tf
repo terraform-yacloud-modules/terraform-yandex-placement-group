1a9190485b4aab3232d1324122b89fb13a49a259
Anton <patsev.anton@gmail.com>
2025-10-25 16:51:40 +0600

    Add placement strategy and timeouts support

--- examples/main.tf
+++ examples/main.tf
@@ -10,4 +10,18 @@ module "placement_group" {
     project     = "my-awesome-project"
     department  = "infrastructure"
   }
+
+  # Стратегия размещения - раскидывать по разным хостам
+  placement_strategy_spread = true
+
+  # Альтернативно: стратегия разделения на партиции
+  # placement_strategy_partitions = 3
+
+  # Таймауты операций
+  timeouts = {
+    create = "10m"
+    delete = "5m"
+    update = "5m"
+  }
+
 }
